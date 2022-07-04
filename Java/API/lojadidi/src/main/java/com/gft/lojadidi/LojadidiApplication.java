e3fba8912b3fab9811df12208d8fae913a181733
LSZA <luis.zancanela@gft.com>
2022-07-04 14:25:10 -0300

    feat: add Java/API/lojadidi with project for API study

---
+++ Java/API/lojadidi/src/main/java/com/gft/lojadidi/LojadidiApplication.java
@@ -0,0 +1,13 @@
+package com.gft.lojadidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojadidiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojadidiApplication.class, args);
+	}
+
+}
---
+++ Java/API/lojadidi/src/test/java/com/gft/lojadidi/LojadidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.lojadidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojadidiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
