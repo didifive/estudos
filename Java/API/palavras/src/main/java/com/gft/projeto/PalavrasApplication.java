75a12615bf38e2790869861f443b8f271d98b5c8
LSZA <luis.zancanela@gft.com>
2022-07-06 11:42:31 -0300

    FIRST COMMIT - criação do projeto Palavras
    Projeto Palavras para treinar conceitos de API aprendidos na semana de
    estudos de API do programa start da GFT

---
+++ Java/API/palavras/src/main/java/com/gft/projeto/PalavrasApplication.java
@@ -0,0 +1,13 @@
+package com.gft.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalavrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalavrasApplication.class, args);
+	}
+
+}
