a2e031c974896bef016579b7a3b453dbdfdfabab
LSZA <luis.zancanela@gft.com>
2022-07-06 23:23:13 -0300

    fat: update package name from projeto to palavrasapi

--- Java/API/palavras/src/main/java/com/gft/palavrasapi/PalavrasApplication.java
+++ Java/API/palavras/src/main/java/com/gft/palavrasapi/PalavrasApplication.java
@@ -1,4 +1,4 @@
-package com.gft.projeto;
+package com.gft.palavrasapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Java/API/palavras/src/test/java/com/gft/palavrasapi/PalavrasApplicationTests.java
+++ Java/API/palavras/src/test/java/com/gft/palavrasapi/PalavrasApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gft.projeto;
+package com.gft.palavrasapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
